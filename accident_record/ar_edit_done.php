f590801c644767baa1e411e68289062b559e9225
KCME-PJ <ken-fujita@kcme.jp>
2019-02-28 19:07:37 +0900

    ファイル追加

---
+++ accident_record/ar_edit_done.php
@@ -0,0 +1,40 @@
+﻿<?php
+session_start();
+require_once('../common/database.php');
+
+if (!isset($_SESSION['join'])) {
+    header('Location: ../user_login.php');
+    exit();
+}
+
+$id = htmlspecialchars($_POST['id']);
+$injury =  htmlspecialchars($_POST['injury']);
+$information =  htmlspecialchars($_POST['information']);
+$traffic =  htmlspecialchars($_POST['traffic']);
+$quality =  htmlspecialchars($_POST['quality']);
+$rmn_pj =  htmlspecialchars($_POST['rmn']);
+
+try {
+    $dbh = getDb();
+    $sql = <<<SQL
+
+UPDATE accident_record
+SET injury=?, information=?, traffic=?, quality=?, rmn_pj=?
+WHERE id=?
+SQL;
+
+    $stmt = $dbh->prepare($sql);
+    $stmt->bindValue(1, $injury, PDO::PARAM_STR);
+    $stmt->bindValue(2, $information, PDO::PARAM_STR);
+    $stmt->bindValue(3, $traffic, PDO::PARAM_STR);
+    $stmt->bindValue(4, $quality, PDO::PARAM_STR);
+    $stmt->bindValue(5, $rmn_pj, PDO::PARAM_STR);
+    $stmt->bindValue(6, $id, PDO::PARAM_INT);
+    
+    $stmt->execute();
+} catch (PDOException $e) {
+    print "ERR! : {$e->getMessage()}";
+} finally {
+    $dbh = null;
+}
+    header('Location: ./ar_list.php');
---
+++ common/database.php
@@ -0,0 +1,14 @@
+﻿<?php
+function getDb()
+{
+    $dsn = 'mysql:dbname=anzen; host=localhost; charset=utf8';
+    $user = 'root';
+    $password = 'anzen';
+    $option = array (
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_CASE => PDO::CASE_NATURAL,
+        PDO::ATTR_PERSISTENT => true
+    );
+        $dbh = new PDO($dsn, $user, $password, $option);
+    return $dbh;
+}
